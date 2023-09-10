526c5bfd7cde2bd14e3c336339a75673945b8315
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-09-10 13:41:06 -0400

    Auto-formatting code

--- main.py
+++ main.py
@@ -17,15 +17,15 @@ from reportlab.pdfgen import canvas
 data = pd.read_csv("customer_behavior_data.csv")
 
 # Data Cleaning and Preprocessing
-data.dropna(inplace=True) # Remove rows with missing values
-data = data[~data.duplicated()] # Remove duplicate rows
+data.dropna(inplace=True)  # Remove rows with missing values
+data = data[~data.duplicated()]  # Remove duplicate rows
 
 # Feature Engineering
 # Perform additional feature engineering steps such as one-hot encoding, scaling, etc. based on your specific data
 
 
 # Customer Segmentation
-X = data.drop('customer_id', axis=1) # Exclude customer_id column if present
+X = data.drop('customer_id', axis=1)  # Exclude customer_id column if present
 
 # Scale the input features for clustering
 scaler = StandardScaler()
@@ -44,7 +44,8 @@ data['cluster_label_dbscan'] = dbscan.labels_
 
 # RNN Model
 # Split data into train and test sets
-X_train, X_test, y_train, y_test = train_test_split(X_scaled, data['target'], test_size=0.2, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(
+    X_scaled, data['target'], test_size=0.2, random_state=0)
 
 # Define the RNN model architecture
 model = Sequential()
@@ -52,16 +53,19 @@ model.add(LSTM(128, input_shape=(X_train.shape[1], 1)))
 model.add(Dense(1, activation='sigmoid'))
 
 # Compile the model
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='binary_crossentropy',
+              optimizer='adam', metrics=['accuracy'])
 
 # Train the model with early stopping
-early_stopping = EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True)
+early_stopping = EarlyStopping(
+    monitor='val_loss', patience=3, restore_best_weights=True)
 history = model.fit(X_train.reshape((X_train.shape[0], X_train.shape[1], 1)), y_train,
                     validation_split=0.2, epochs=10, batch_size=32, callbacks=[early_stopping])
 
 
 # Customer Behavior Analysis
-predicted_classes = model.predict_classes(X_test.reshape((X_test.shape[0], X_test.shape[1], 1)))
+predicted_classes = model.predict_classes(
+    X_test.reshape((X_test.shape[0], X_test.shape[1], 1)))
 
 # Evaluate model accuracy
 accuracy = accuracy_score(y_test, predicted_classes)
@@ -69,7 +73,8 @@ conf_matrix = confusion_matrix(y_test, predicted_classes)
 
 
 # Prediction of Customer Actions
-future_actions = model.predict_classes(future_data_scaled.reshape((future_data_scaled.shape[0], future_data_scaled.shape[1], 1)))
+future_actions = model.predict_classes(future_data_scaled.reshape(
+    (future_data_scaled.shape[0], future_data_scaled.shape[1], 1)))
 
 
 # Statistical Analysis and Visualization
