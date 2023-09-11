c2aa019298c2878ca6236fd4d398fc4aa09b46e2
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-09-10 21:04:12 -0400

    AI-enhanced and auto-formatted code

--- main.py
+++ main.py
@@ -1,98 +1,96 @@
-import pandas as pd
-import numpy as np
-from sklearn.preprocessing import StandardScaler
-from sklearn.cluster import KMeans, DBSCAN
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, confusion_matrix
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense
-from tensorflow.keras.callbacks import EarlyStopping
-import matplotlib.pyplot as plt
-import seaborn as sns
-from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
+import seaborn as sns
+import matplotlib.pyplot as plt
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras.models import Sequential
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.cluster import KMeans, DBSCAN
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+import pandas as pd
+The Python script has been optimized. Here are the optimized versions of the script:
+
+```python
+
+
+def data_cleaning(data):
+    # Remove rows with missing values
+    data.dropna(inplace=True)
+    # Remove duplicate rows
+    data = data[~data.duplicated()]
+    return data
+
+
+def customer_segmentation(data):
+    X = data.drop('customer_id', axis=1)
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
+    kmeans = KMeans(n_clusters=5, random_state=0)
+    kmeans.fit(X_scaled)
+    data['cluster_label'] = kmeans.labels_
+    dbscan = DBSCAN(eps=0.5, min_samples=5)
+    dbscan.fit(X_scaled)
+    data['cluster_label_dbscan'] = dbscan.labels_
+    return data
+
+
+def rnn_model(X_train, y_train):
+    model = Sequential()
+    model.add(LSTM(128, input_shape=(X_train.shape[1], 1)))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(loss='binary_crossentropy',
+                  optimizer='adam', metrics=['accuracy'])
+    early_stopping = EarlyStopping(
+        monitor='val_loss', patience=3, restore_best_weights=True)
+    history = model.fit(X_train.reshape((X_train.shape[0], X_train.shape[1], 1)), y_train,
+                        validation_split=0.2, epochs=10, batch_size=32, callbacks=[early_stopping])
+    return model
+
+
+def evaluate_model(model, X_test, y_test):
+    predicted_classes = model.predict_classes(
+        X_test.reshape((X_test.shape[0], X_test.shape[1], 1)))
+    accuracy = accuracy_score(y_test, predicted_classes)
+    conf_matrix = confusion_matrix(y_test, predicted_classes)
+    return accuracy, conf_matrix
 
 
 # Read customer behavior data
 data = pd.read_csv("customer_behavior_data.csv")
 
 # Data Cleaning and Preprocessing
-data.dropna(inplace=True)  # Remove rows with missing values
-data = data[~data.duplicated()]  # Remove duplicate rows
-
-# Feature Engineering
-# Perform additional feature engineering steps such as one-hot encoding, scaling, etc. based on your specific data
-
+data = data_cleaning(data)
 
 # Customer Segmentation
-X = data.drop('customer_id', axis=1)  # Exclude customer_id column if present
-
-# Scale the input features for clustering
-scaler = StandardScaler()
-X_scaled = scaler.fit_transform(X)
-
-# K-means clustering
-kmeans = KMeans(n_clusters=5, random_state=0)
-kmeans.fit(X_scaled)
-data['cluster_label'] = kmeans.labels_
-
-# DBSCAN clustering
-dbscan = DBSCAN(eps=0.5, min_samples=5)
-dbscan.fit(X_scaled)
-data['cluster_label_dbscan'] = dbscan.labels_
-
+data = customer_segmentation(data)
 
 # RNN Model
-# Split data into train and test sets
+X = data.drop('target', axis=1)
+y = data['target']
 X_train, X_test, y_train, y_test = train_test_split(
-    X_scaled, data['target'], test_size=0.2, random_state=0)
+    X, y, test_size=0.2, random_state=0)
+model = rnn_model(X_train, y_train)
 
-# Define the RNN model architecture
-model = Sequential()
-model.add(LSTM(128, input_shape=(X_train.shape[1], 1)))
-model.add(Dense(1, activation='sigmoid'))
-
-# Compile the model
-model.compile(loss='binary_crossentropy',
-              optimizer='adam', metrics=['accuracy'])
-
-# Train the model with early stopping
-early_stopping = EarlyStopping(
-    monitor='val_loss', patience=3, restore_best_weights=True)
-history = model.fit(X_train.reshape((X_train.shape[0], X_train.shape[1], 1)), y_train,
-                    validation_split=0.2, epochs=10, batch_size=32, callbacks=[early_stopping])
-
-
-# Customer Behavior Analysis
-predicted_classes = model.predict_classes(
-    X_test.reshape((X_test.shape[0], X_test.shape[1], 1)))
-
-# Evaluate model accuracy
-accuracy = accuracy_score(y_test, predicted_classes)
-conf_matrix = confusion_matrix(y_test, predicted_classes)
-
+# Evaluation
+accuracy, conf_matrix = evaluate_model(model, X_test, y_test)
 
 # Prediction of Customer Actions
+future_data_scaled = scaler.transform(future_data)
 future_actions = model.predict_classes(future_data_scaled.reshape(
     (future_data_scaled.shape[0], future_data_scaled.shape[1], 1)))
 
-
 # Statistical Analysis and Visualization
-# Perform statistical tests and generate visualizations based on your specific data and requirements
-
 
 # Report Generation
-# Generate a comprehensive report summarizing findings, statistical analyses, visualizations, and recommendations
-
-# Create PDF canvas
 report = canvas.Canvas("customer_behavior_report.pdf", pagesize=letter)
-
-# Set font and font size
 report.setFont("Helvetica", 12)
-
-# Contents of the report
 report.drawString(50, 800, "Customer Behavior Analysis and Prediction Report")
 report.drawString(50, 780, "Summary:")
 # Add a summary of the findings and recommendations
-
 report.save()
+```
+
+Please make sure to replace any necessary variables and functions, as they might have been omitted from the provided code snippet.
